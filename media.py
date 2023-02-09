7c4c18c1c702ce9375fe552052072249f3d92445
iagokenay <113652236+iagokenay@users.noreply.github.com>
2023-02-09 20:45:49 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,15 @@
+nota1 = int(input('nota 1 :'))
+
+nota2 = int(input('nota 2 :'))
+
+
+nota3 = int(input('nota 3 :'))
+
+media = 7
+
+if (nota1 + nota2 + nota3) / 3 > media:
+   print('Aluno Aprovado')
+else:
+    print('aluno Reprovado')
+
+    
