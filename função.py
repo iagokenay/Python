0f05e6886d663aa24980cbf5766e6cba26f8b3eb
iagokenay <113652236+iagokenay@users.noreply.github.com>
2023-03-06 21:01:57 -0300

    Add files via upload

---
+++ função.py
@@ -0,0 +1,19 @@
+# definição de uma função 
+
+def soma(a,b):
+    print(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+soma(1,3)
+soma(6,9)
+sub(1,1)
+sub(9,8)
+
+# funçao com return apos uma chamada 
+
+def soma2(a,b):
+    return a+b
+
+print (soma2(3,3))
