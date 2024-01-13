8a76e538346e40766a98c7fc8e71af478827195a
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-01-12 21:15:12 -0300

    Add files via upload

---
+++ bee3e4.py
@@ -0,0 +1,30 @@
+#text beecrowd 
+# A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+
+# - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+# Entrada
+# A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+# Saída
+# Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal.
+# Utilize variáveis de dupla precisão (double). 
+# Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+# n = 3.14159
+# raio=float(input ())
+# area = raio**2 *n
+# print(f"A=%.4f{%area}")
+# Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+# A seguir escrever o valor desta variável.
+# Entrada
+# O arquivo de entrada contém 2 valores inteiros.
+# Saída
+# Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor 
+# correspondente à soma de A e B. 
+# Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+A = int(input())
+B = int(input())
+soma = A+B
+print(f"SOMA = {soma}")
