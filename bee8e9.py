272d3eade9b310b55ebbfaab053299f9ec7c5d18
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-01-15 23:21:43 -0300

    Add files via upload

---
+++ bee8e9.py
@@ -0,0 +1,24 @@
+# Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+# o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário,
+# com duas casas decimais.
+
+# Entrada
+# O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, 
+# quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+
+# Saída
+# Imprima o número e o salário do funcionário, conforme exemplo fornecido,
+# com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+# N = int(input())
+# N2 = int(input())
+# N3 = float(input())
+# salario = N2*N3
+# print(f"NUMBER = {N}")
+# print("SALARY = U$ %.2f"%salario)
+
+nome = input()
+salario = float(input())
+vendas = float(input())
+por = vendas * 0.15 
+total = por + salario
+print( "TOTAL = R$%.2f"%total)
