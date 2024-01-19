04bf17c76719a889ca987afde0c22c76ae6fa01b
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-01-19 17:25:16 -0300

    Add files via upload

---
+++ bee12.py
@@ -0,0 +1,40 @@
+# Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+# a) a área do triângulo retângulo que tem A por base e C por altura.
+# b) a área do círculo de raio C. (pi = 3.14159)
+# c) a área do trapézio que tem A e B por bases e C por altura.
+# d) a área do quadrado que tem lado B.
+# e) a área do retângulo que tem lados A e B.
+# Entrada
+# O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+
+# Saída
+# O arquivo de saída deverá conter 5 linhas de dados. 
+# Cada linha corresponde a uma das áreas descritas acima,
+# sempre com mensagem correspondente e um espaço entre os dois pontos e o valor.
+# O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+for i in range(1):
+    item = input().split(" ")
+    A =float(item[0])
+    B =float(item[1])
+    C =float(item[2])
+
+pi = 3.14159
+# a) a área do triângulo retângulo que tem A por base e C por altura.
+area1 = (A*C)/2
+# b) a área do círculo de raio C. (pi = 3.14159)
+area2 = pi*C**2
+# c) a área do trapézio que tem A e B por bases e C por altura.
+area3 = (A+B)*C /2
+# d) a área do quadrado que tem lado B.
+area4 = B**2
+# e) a área do retângulo que tem lados A e B.
+area5 = A*B
+
+
+print("TRIANGULO: %.3f"%area1)
+print("CIRCULO: %.3f"%area2)
+print("TRAPEZIO: %.3f"%area3)
+print("QUADRADO: %.3f"%area4)
+print("RETANGULO: %.3f"%area5)
+
+
