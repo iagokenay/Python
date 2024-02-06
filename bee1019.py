0e4dc20db77d89367feba3c49d2c0cda597356b9
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-02-05 21:08:46 -0300

    Add files via upload

---
+++ bee1019.py
@@ -0,0 +1,20 @@
+
+# Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
+# e informe-o expresso no formato horas:minutos:segundos.
+
+# Entrada
+# O arquivo de entrada contém um valor inteiro N.
+
+# Saída
+# Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+
+
+segundos = int(input()) #recebo valor em segundos 
+
+horas = segundos  //(60*60)
+segundos = segundos%(60*60)
+
+minutos = segundos // 60
+segundos = segundos%60
+
+print(f"{horas}:{minutos}:{segundos}")
